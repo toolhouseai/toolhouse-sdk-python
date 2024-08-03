63af51ab61c98d0565431b7700a2746c25450631
Fabio Pulvirenti <fabio@etiqa.it>
2024-08-03 14:06:04 +0200

    fix: change the name of the function to highlight that the name can be any function name.

--- examples/sample_run_local_tools.py
+++ examples/sample_run_local_tools.py
@@ -34,7 +34,7 @@ th.set_metadata("timezone", 5)
 
 
 @th.register_local_tool("hello")
-def hello(city: str):
+def hello_tool(city: str):
     """Return Local Time"""
     return f"Hello from {city}!!!"
 
